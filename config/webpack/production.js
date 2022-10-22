5b2c24698fc67b219bb5474abea38ed16794d02a
Gabriel Mathias <gablemathias@icloud.com>
2022-10-22 17:38:32 -0300

    webpack

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,11 +1,5 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
-<<<<<<< HEAD
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
-=======
-const webpackConfig = require('./base')
-
-module.exports = webpackConfig
->>>>>>> 995e08f2e0e60c9f7950b4fc3112aed9c801608d
