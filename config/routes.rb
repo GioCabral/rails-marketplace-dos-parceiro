fae54223239841e2525fb4d58f19934d64e90a30
giovanni cabral <giolucacab@outlook.com>
2022-10-22 20:35:06 -0300

    gg

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :feelings
+  resources :feelings do
+    resources :orders, only: %i[ new create]
+  end
+  resources :orders, onnly: %i[index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+user = User.create(email: 'batatinha@batata.com', password: 'batata')
+user_1 = User.create(email: 'abobrinha@batata.com', password: 'batata')
+user_2 = User.create(email: 'bananinha@batata.com', password: 'batata')
+
+feeling = Feeling.create(name: 'Calm', price: 100.00, user_id: user.id)
+feeling_1 = Feeling.create(name: 'Angry', price: 100.00, user_id: user_1.id)
+feeling_2 = Feeling.create(name: 'Peace', price: 100.00, user_id: user_2.id)
