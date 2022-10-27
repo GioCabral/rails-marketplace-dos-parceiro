12f127432fa15b9ed376b4331c9fa9d930403188
giovanni cabral <giolucacab@outlook.com>
2022-10-27 19:45:39 -0300

    root is index

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "feelings#index"
 
   resources :feelings do
     resources :orders, only: %i[new create]
   end
   resources :orders, only: %i[index]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
