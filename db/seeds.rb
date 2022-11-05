70c700ff9fad852c4e04e65781e10a4323de1e5f
giovanni cabral <giolucacab@outlook.com>
2022-11-05 14:17:51 -0300

    esqueci oq esse commit fez

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-user = User.create(email: 'batatinha@batata.com', password: 'batata')
-user_1 = User.create(email: 'abobrinha@batata.com', password: 'batata')
-user_2 = User.create(email: 'bananinha@batata.com', password: 'batata')
+Order.destroy_all
+Feeling.destroy_all
+User.destroy_all
 
-feeling = Feeling.create(name: 'Calm', description: "Consegui esse sentimento ao assistir o show do milhão" , percentage: 100, user_id: user.id)
-feeling_1 = Feeling.create(name: 'Angry', description: "bolo de milho" ,percentage: 100, user_id: user_1.id)
-feeling_2 = Feeling.create(name: 'Peace', description: "This generator can do much more than append a timest", percentage: 100, user_id: user_2.id)
+idx = 0
+
+100.times do
+  user = User.create!(email: "#{idx += 1 }@batata.com", password: 'batata')
+
+  feeling = Feeling.create!(name: Faker::Emotion.noun,
+                            description: "Consegui esse sentimento ao assistir o show do milhão" ,
+                            percentage: 100,
+                            user_id: user.id)
+
+    file = URI.open("http://source.unsplash.com/random/#{feeling.name}")
+    feeling.photo.attach(io: file, filename: "#{feeling.name}.png", content_type: "image/png")
+    feeling.save!
+
+end
